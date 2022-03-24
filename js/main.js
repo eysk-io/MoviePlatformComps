a95d29c2174e47acff7f83b72249be3113f2ec12
UBC Student <eykim@students.cs.ubc.ca>
2022-03-23 19:07:22 -0700

    Refactor main.js and add linting exceptions (#9)

--- js/main.js
+++ js/main.js
@@ -1,26 +1,7 @@
-let data;
-let barData;
-let innovationChart;
-let barChart;
-let pieChart;
-
-d3.csv('data/groupedPlatformGenre.csv')
-      .then((_barData) => {
-      barData = _barData;
-
-      console.log(barData);
-});
-
 d3.csv('data/groupByPlatform.csv')
   .then((_data) => {
     const data = collapseCategories(_data);
-    console.log(_data);
-
-    let groupedPlatformGenre = d3.rollups(data, v => v.length, d => d.platform + '-' + d.genre);
-    let dataPlatformGenre = Array.from(groupedPlatformGenre, ([key, count]) => ({ key, count }));
-  
-    dataPlatformGenre.sort((a, b) => b.count - a.count);
-    console.log(dataPlatformGenre);
+    data.sort((a, b) => a.label - b.label);
 
     const innovationChart = new InnovationChart({
       parentElement: '#innovation-chart',
@@ -36,7 +17,6 @@ d3.csv('data/groupByPlatform.csv')
       parentElement: '#bar-chart',
     }, barData, dataPlatformGenre);
     barChart.updateVis();
-
   });
 
 function collapseCategories(_data) {
