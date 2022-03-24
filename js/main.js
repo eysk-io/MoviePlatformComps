67fad68a8de5f70ce22307b2d1318f2cd0ff29ec
UBC Student <eykim@students.cs.ubc.ca>
2022-03-23 19:07:22 -0700

    Refactor main.js and add linting exceptions (#9)

--- js/main.js
+++ js/main.js
@@ -1,49 +1,43 @@
-let data;
-let innovationChart;
-let barChart;
-let pieChart;
-
 d3.csv('data/groupByPlatform.csv')
   .then((_data) => {
-    data = _data;
-
-    // collapse small genre categories to 'Other'
-    data.forEach(d => {
-      Object.keys(d).forEach(attr => {
-        if (attr == 'genre') {
-          // d[attr] = (d[attr] == 'NA') ? null : +d[attr];
-          switch (d[attr]) {
-            case 'Fantasy': 
-              d[attr] = 'Other';
-            case 'Mystery': 
-              d[attr] = 'Other';
-              case 'Family': 
-              d[attr] = 'Other';
-            case 'Thriller': 
-              d[attr] = 'Other';
-            case 'Sport': 
-              d[attr] = 'Other';
-            case 'Sci-Fi': 
-              d[attr] = 'Other';  
-          }
-        } 
-      });
-    });
-
+    const data = collapseCategories(_data);
     data.sort((a, b) => a.label - b.label);
 
-    innovationChart = new InnovationChart({
+    const innovationChart = new InnovationChart({
       parentElement: '#innovation-chart',
     }, data);
     innovationChart.updateVis();
 
-    pieChart = new PieChart({
+    const pieChart = new PieChart({
       parentElement: '#pie-chart',
     }, data);
     pieChart.updateVis();
 
-    barChart = new BarChart({
+    const barChart = new BarChart({
       parentElement: '#bar-chart',
     }, data);
     barChart.updateVis();
   });
+
+function collapseCategories(_data) {
+  const data = _data;
+
+  data.forEach((d) => {
+    Object.keys(d).forEach((attr) => {
+      if (attr === 'genre') {
+        switch (d[attr]) {
+          case 'Fantasy':
+          case 'Mystery':
+          case 'Family':
+          case 'Thriller':
+          case 'Sport':
+          case 'Sci-Fi':
+          default:
+            d[attr] = 'Other';
+        }
+      }
+    });
+  });
+
+  return data;
+}
