878f4b7a056b1c22ffbc8eb3f04bb92fe4cb890e
johnnybcs <johnny@ualberta.ca>
2022-03-25 11:39:36 -0700

    error explosion but on the bright side its grouping by platform

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 let barData;
 
-d3.csv('data/groupedPlatformGenre.csv')
+d3.csv('data/preprocessedMovies2.csv')
   .then((_barData) => {
     barData = _barData;
   });
 
-d3.csv('data/groupByPlatform.csv')
+d3.csv('data/preprocessedMovies2.csv')
   .then((_data) => {
-    const data = collapseCategories(_data);
+    let data = collapseCategories(_data);
+    data = groupByPlatform(data);
     let groupedPlatformGenre = d3.rollups(data, v => v.length, d => d.platform + '-' + d.genre);
     let dataPlatformGenre = Array.from(groupedPlatformGenre, ([key, count]) => ({ key, count }));
 
@@ -49,4 +50,31 @@ function collapseCategories(_data) {
   });
 
   return data;
+}
+
+function groupByPlatform(_data) {
+  const data = _data;
+  let groupByPlatformData = []
+
+  data.forEach((d) => {
+    Object.keys(d).forEach((attr) => {
+      if (attr === 'Netflix' && d[attr] == '1') {
+        d['platform'] = 'Netflix'
+        groupByPlatformData.push(d)
+      }
+      if (attr === 'Hulu' && d[attr] == '1') {
+        d['platform'] = 'Hulu'
+        groupByPlatformData.push(d)
+      }
+      if (attr === 'Prime Video' && d[attr] == '1') {
+        d['platform'] = 'Prime Video'
+        groupByPlatformData.push(d)
+      }
+      if (attr === 'Disney+' && d[attr] == '1') {
+        d['platform'] = 'Disney+'
+        groupByPlatformData.push(d)
+      }
+    });
+  });
+  return groupByPlatformData;
 }
