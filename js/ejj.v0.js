abc8ed01ec4424fe19256b2e023216a4ace76d7a
Eric Kim <erickim195@gmail.com>
2022-04-05 14:30:09 -0700

    Abstract out ejj lib

---
+++ js/ejj.v0.js
@@ -0,0 +1,123 @@
+function getAllGenres(data) {
+  const allGenres = new Set();
+
+  data.forEach((d) => {
+    allGenres.add(d.genre);
+  });
+
+  return allGenres;
+}
+
+function getAllMpaa(data) {
+  const allMpaa = new Set();
+
+  data.forEach((d) => {
+    allMpaa.add(d.rating);
+  });
+
+  return allMpaa;
+}
+
+function getAllPlatforms(data) {
+  const allPlatforms = new Set();
+
+  data.forEach((d) => {
+    allPlatforms.add(d.platform);
+  });
+
+  return allPlatforms;
+}
+
+function removeChildren(elts) {
+  elts.forEach((elt) => {
+    while (elt.firstChild) {
+      elt.removeChild(elt.firstChild);
+    }
+  });
+}
+
+// https://stackoverflow.com/questions/11805251/add-html-elements-dynamically-with-javascript-inside-div-with-specific-id
+function createFrag(htmlStr) {
+  const frag = document.createDocumentFragment();
+  const temp = document.createElement('div');
+  temp.innerHTML = htmlStr;
+  while (temp.firstChild) {
+    frag.appendChild(temp.firstChild);
+  }
+  return frag;
+}
+
+function generateMpaRatingWidgets(_data) {
+  const data = _data;
+  let widgets = new Set();
+
+  data.forEach((d) => widgets.add(d.rating));
+  widgets = Array.from(widgets);
+  widgets.sort();
+
+  widgets.forEach((w) => {
+    const button = createFrag(`<button class="mpa-rating-button widget">${w}</button>`);
+    document.getElementById('mpa-rating-button-container').appendChild(button);
+  });
+}
+
+function filterBySelected(filterVal, selected, dataObj) {
+  if (dataObj.allGenres.has(filterVal)) {
+    if (selected.genres.includes(filterVal)) {
+      const idx = selected.genres.indexOf(filterVal);
+      selected.genres.splice(idx, 1);
+    } else {
+      selected.genres.push(filterVal);
+    }
+  } else if (dataObj.allMpaa.has(filterVal)) {
+    if (selected.mpaa.includes(filterVal)) {
+      const idx = selected.mpaa.indexOf(filterVal);
+      selected.mpaa.splice(idx, 1);
+    } else {
+      selected.mpaa.push(filterVal);
+    }
+  } else if (dataObj.allPlatforms.has(filterVal)) {
+    if (selected.platforms.includes(filterVal)) {
+      const idx = selected.platforms.indexOf(filterVal);
+      selected.platforms.splice(idx, 1);
+    } else {
+      selected.platforms.push(filterVal);
+    }
+  }
+
+  const filtered = dataObj.rawData.filter((d) => {
+    let genreExists = false;
+    selected.genres.forEach((s) => {
+      if (Object.values(d).includes(s)) {
+        genreExists = true;
+      }
+    });
+
+    let mpaaExists = false;
+    selected.mpaa.forEach((s) => {
+      if (Object.values(d).includes(s)) {
+        mpaaExists = true;
+      }
+    });
+
+    let platformExists = false;
+    selected.platforms.forEach((s) => {
+      if (Object.values(d).includes(s)) {
+        platformExists = true;
+      }
+    });
+
+    return genreExists && mpaaExists && platformExists;
+  });
+
+  return filtered;
+}
+
+const ejj = {
+  getAllGenres,
+  getAllMpaa,
+  getAllPlatforms,
+  removeChildren,
+  generateMpaRatingWidgets,
+  filterBySelected,
+};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const utils = functions;
+const ejjLib = ejj;
 const selected = {
   genres: [],
   mpaa: [],
@@ -13,9 +14,9 @@ d3.csv('data/preprocessedMovies2.csv')
     return rawData;
   })
   .then((rawData) => {
-    const allGenreSet = getAllGenres(rawData);
-    const allMpaaSet = getAllMpaa(rawData);
-    const allPlatformsSet = getAllPlatforms(rawData);
+    const allGenreSet = ejjLib.getAllGenres(rawData);
+    const allMpaaSet = ejjLib.getAllMpaa(rawData);
+    const allPlatformsSet = ejjLib.getAllPlatforms(rawData);
 
     selected.genres = Array.from(allGenreSet);
     selected.mpaa = Array.from(allMpaaSet);
@@ -64,60 +65,14 @@ function renderCharts(dataObj) {
   // barChart.updateVis();
 }
 function renderAll(dataObj) {
-  generateMpaRatingWidgets(dataObj.rawData);
+  ejjLib.generateMpaRatingWidgets(dataObj.rawData);
   renderCharts(dataObj);
 
   const widgets = document.querySelectorAll('.widget');
   widgets.forEach((elt) => {
     elt.addEventListener('click', (e) => {
       const filterVal = e.target.innerHTML;
-      if (dataObj.allGenres.has(filterVal)) {
-        if (selected.genres.includes(filterVal)) {
-          const idx = selected.genres.indexOf(filterVal);
-          selected.genres.splice(idx, 1);
-        } else {
-          selected.genres.push(filterVal);
-        }
-      } else if (dataObj.allMpaa.has(filterVal)) {
-        if (selected.mpaa.includes(filterVal)) {
-          const idx = selected.mpaa.indexOf(filterVal);
-          selected.mpaa.splice(idx, 1);
-        } else {
-          selected.mpaa.push(filterVal);
-        }
-      } else if (dataObj.allPlatforms.has(filterVal)) {
-        if (selected.platforms.includes(filterVal)) {
-          const idx = selected.platforms.indexOf(filterVal);
-          selected.platforms.splice(idx, 1);
-        } else {
-          selected.platforms.push(filterVal);
-        }
-      }
-
-      const filtered = dataObj.rawData.filter((d) => {
-        let genreExists = false;
-        selected.genres.forEach((s) => {
-          if (Object.values(d).includes(s)) {
-            genreExists = true;
-          }
-        });
-
-        let mpaaExists = false;
-        selected.mpaa.forEach((s) => {
-          if (Object.values(d).includes(s)) {
-            mpaaExists = true;
-          }
-        });
-
-        let platformExists = false;
-        selected.platforms.forEach((s) => {
-          if (Object.values(d).includes(s)) {
-            platformExists = true;
-          }
-        });
-
-        return genreExists && mpaaExists && platformExists;
-      });
+      const filtered = ejjLib.filterBySelected(filterVal, selected, dataObj);
 
       dataObj = { ...dataObj, data: filtered };
 
@@ -128,72 +83,9 @@ function renderAll(dataObj) {
         document.getElementById('mpa-rating-button-container'),
       ];
 
-      removeChildren(allElts);
+      ejjLib.removeChildren(allElts);
 
       renderAll(dataObj);
     });
   });
-}
-
-function removeChildren(elts) {
-  elts.forEach((elt) => {
-    while (elt.firstChild) {
-      elt.removeChild(elt.firstChild);
-    }
-  });
-}
-
-function generateMpaRatingWidgets(_data) {
-  const data = _data;
-  let widgets = new Set();
-
-  data.forEach((d) => widgets.add(d.rating));
-  widgets = Array.from(widgets);
-  widgets.sort();
-
-  widgets.forEach((w) => {
-    const button = createFrag(`<button class="mpa-rating-button widget">${w}</button>`);
-    document.getElementById('mpa-rating-button-container').appendChild(button);
-  });
-}
-
-// https://stackoverflow.com/questions/11805251/add-html-elements-dynamically-with-javascript-inside-div-with-specific-id
-function createFrag(htmlStr) {
-  const frag = document.createDocumentFragment();
-  const temp = document.createElement('div');
-  temp.innerHTML = htmlStr;
-  while (temp.firstChild) {
-    frag.appendChild(temp.firstChild);
-  }
-  return frag;
-}
-
-function getAllGenres(data) {
-  const allGenres = new Set();
-
-  data.forEach((d) => {
-    allGenres.add(d.genre);
-  });
-
-  return allGenres;
-}
-
-function getAllMpaa(data) {
-  const allMpaa = new Set();
-
-  data.forEach((d) => {
-    allMpaa.add(d.rating);
-  });
-
-  return allMpaa;
-}
-
-function getAllPlatforms(data) {
-  const allPlatforms = new Set();
-
-  data.forEach((d) => {
-    allPlatforms.add(d.platform);
-  });
-
-  return allPlatforms;
 }
